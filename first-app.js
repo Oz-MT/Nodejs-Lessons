4eee9566496c7ab39a1eb61fcafaf2793783ec00
Beyza Öztekin <mattsoftwareng@gmail.com>
2022-09-06 17:04:01 +0300

    module-nedir

--- first-app.js
+++ first-app.js
@@ -1,3 +1,5 @@
+const myModule = require("./module-nedir"); // Buna erişirken sonuna js de desek olur demesek de. Başka bir module'e bağlantıyı sağlıyor. import etmek gibi düşübiliriz require'ı.
+
 function selamVer() {
   console.log("Merhaba Node JS");
   //   console.log(window);
@@ -5,7 +7,19 @@ function selamVer() {
 }
 
 selamVer();
+console.log(myModule.ad);
+myModule.ekle(5, 10);
+myModule.cikar(52, 42);
+console.log(myModule.personel);
+console.log(myModule.personel.yas);
+
+//? module-nedir.js dosyasından hiçbir şey export etmeden doğrudan başka bir dosyada değişkeni çağıramayız, hata verir.
+//? Haberdar edebilmek için bazı yöntemlerimiz vardır.
 
 //! Terminal--> New Terminal-->powershell içinde: "node .\first-app.js" yazdık. Böylece borwser dışında bir ortamda (bu server da olabilirdi) çalışabiliyor.
 
 //! window nesnesi browser'da tanımlıdır fakat browser dışında tanımlı değildir. Hata alırız.
+
+//! window yerine global kullanabiliriz ve console.log ile elde ettiğimiz sonucun aynısını alabiliriz.
+
+//! Eğer require kısmında let ile tanımlasaydık farkında olmadan farklı bir değer atayabilirdik. Bu yüzden sabit olan const'atarız.
--- module-nedir.js
+++ module-nedir.js
@@ -7,9 +7,25 @@
 // node js uygulamalarında her bir js dosyası bir module'dür. Bu module tanımlanan tüm değişken ve fonksiyonlar bu dosyaya özeldir.(private)
 // Bunlara diğer module'ler üzerinden erişebilmenin yöntemleri vardır.
 
-let isim = "emre";
+let isim = "emre altunbilek";
 let topla = function (a, b) {
   console.log(a + b);
 };
 
+// console.log(module);
+
+module.exports.ad = isim;
+module.exports.ekle = topla;
+exports.cikar = function (a, b) {
+  console.log(a - b);
+};
+exports.personel = {
+  ad: "emre",
+  yas: "32",
+};
 console.log(module);
+
+// Ben buradaki isim ve topla değerlerini ad ve ekle ismi ile dışarıya import ediyorum .
+// Baş kısmında modele kullanmasak da olur.
+// console.log(module) dediğimizde ve çalıştırdığımızda, exports içine ad ve ekle bilgilerini aşağıdaki gibi gönderdiğimizi görürüz.
+// exports: { ad: 'emre altunbilek', ekle: [Function: topla] }
